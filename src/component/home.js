efc37a5ff165fb0f54ca4e96af616df4bebd8334
KashafMughal <kashafmughal824@gmail.com>
2023-03-12 21:13:07 +0500

    para.js component create

--- src/component/home.js
+++ src/component/home.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
-const Home = () => {
+export default function Home() {
   return (
-    <div>Home</div>
-  )
+    <div className="text-black border border-neutral-900">lorem ipsum</div>
+  );
 }
-
-export default Home
